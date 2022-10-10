1ba72998fe8eb671f012fa36dab7e4fef4bb35c8
TAYFUN KAYA <tayfunkaya.uni@gmail.com>
2022-10-10 17:33:45 +0300

    change data

--- src/store/index.js
+++ src/store/index.js
@@ -7,8 +7,11 @@ const STORE = createStore(reducers, {
     {id: 2, name: 'Category 2', product: []},
   ],
   product: [
-    {id: 1, name: 'tayfun', checked: true},
-    {id: 2, name: 'tayfun2', checked: false},
+    {id: 1, name: 'product 1', checked: true},
+    {id: 2, name: 'product 2', checked: false},
+    {id: 3, name: 'product 3', checked: false},
+    {id: 4, name: 'product 4', checked: false},
+    {id: 5, name: 'product 5', checked: false},
   ],
 });
 
